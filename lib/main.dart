ef1ec1d1ff6dc4aad8fd1374610ec3f944683e43
Rodrigo A. Silva <rgbrasil2@gmail.com>
2021-11-21 12:12:05 -0300

    chore: Aplicando Estilo no Container

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,17 @@ class HomeWidget extends StatelessWidget {
                     child: Row(
                       children: [
                         Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(10),
                           child: Text(
                             item.value.toString(),
                           ),
