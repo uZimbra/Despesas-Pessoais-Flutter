c7ac0a4355bf26ef2eaeb1cd0978d98f2be6f9bb
Rodrigo A. Silva <rgbrasil2@gmail.com>
2021-11-23 16:56:11 -0300

    chore: Usando ListView

--- lib/components/transaction_list.widget.dart
+++ lib/components/transaction_list.widget.dart
@@ -14,59 +14,58 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: SingleChildScrollView(
-        physics: const ScrollPhysics(parent: BouncingScrollPhysics()),
-        child: Column(
-          children: transactions
-              .map(
-                (item) => Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${item.value.toStringAsFixed(2)}',
-                          style: const TextStyle(
-                            color: Colors.purple,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            item.title,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(item.date),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+      child: ListView.builder(
+        physics: const ScrollPhysics(
+          parent: BouncingScrollPhysics(),
+        ),
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final item = transactions[index];
+
+          return Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
                   ),
                 ),
-              )
-              .toList(),
-        ),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${item.value.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(item.date),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
